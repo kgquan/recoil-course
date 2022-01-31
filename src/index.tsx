dc310b0b320e3d3ebc855490912d71b09a3e8c68
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2022-01-31 02:41:54 -0700

    Wrap all components in RecoilRoot component

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Canvas from './Canvas'
 import {ChakraProvider} from '@chakra-ui/react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider>
-            <Router>
-                <Switch>
-                    <Route>
-                        <Canvas />
-                    </Route>
-                </Switch>
-            </Router>
-        </ChakraProvider>
+        <RecoilRoot>
+            <ChakraProvider>
+                <Router>
+                    <Switch>
+                        <Route>
+                            <Canvas />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ChakraProvider>
+        </RecoilRoot>
     </React.StrictMode>,
     document.getElementById('root'),
 )
