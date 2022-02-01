b4e489b13b20e7d86706b3e9404dca293fbb4d75
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2022-01-31 21:27:39 -0700

    Add euro selector

--- src/examples/Selectors.tsx
+++ src/examples/Selectors.tsx
@@ -10,7 +10,7 @@ import {
   Switch,
 } from '@chakra-ui/react'
 import {ArrowRight} from 'react-feather'
-import {atom, useRecoilState} from 'recoil'
+import {atom, selector, useRecoilState, useRecoilValue} from 'recoil'
 
 const exchangeRate = 0.83
 
@@ -19,8 +19,17 @@ const usdAtom = atom({
   default: 1
 })
 
+const eurSelector = selector({
+  key: 'eur',
+  get: ({ get }) => {
+    const usd = get(usdAtom)
+    return usd * exchangeRate
+  }
+})
+
 export const Selectors = () => {
   const [usd, setUSD] = useRecoilState(usdAtom);
+  const eur = useRecoilValue(eurSelector);
 
   return (
       <div style={{padding: 20}}>
@@ -29,7 +38,7 @@ export const Selectors = () => {
           </Heading>
           <InputStack>
             <CurrencyInput label="usd" amount={usd} onChange={(usd) => setUSD(usd)}/>
-            <CurrencyInput label="eur" amount={usd * exchangeRate} />
+            <CurrencyInput label="eur" amount={eur} />
           </InputStack>
           <Commission />
       </div>
