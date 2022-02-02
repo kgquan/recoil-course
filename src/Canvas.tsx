eb0f41163589d16e00305ea0f076e38a02cb864d
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2022-02-02 04:45:58 -0700

    Completely replace functionality that uses context with atoms (much of it using atom families)

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,20 +1,7 @@
-import {createContext, useState} from 'react'
 import {Element, Rectangle} from './components/Rectangle/Rectangle'
 import {PageContainer} from './PageContainer'
 import { Toolbar } from './Toolbar'
-import { atom, useSetRecoilState } from 'recoil'
-
-type ElementsContextType = {
-    elements: Element[]
-    addElement: () => void
-    setElement: SetElement
-}
-
-export const ElementsContext = createContext<ElementsContextType>({
-    elements: [],
-    addElement: () => {},
-    setElement: () => {},
-})
+import { atom, useRecoilValue, useSetRecoilState } from 'recoil'
 
 export type SetElement = (indexToSet: number, newElement: Element) => void
 
@@ -23,47 +10,26 @@ export const selectedElementState = atom<number | null>({
     default: null
 })
 
+export const elementsState = atom<number[]>({
+    key: 'elements',
+    default: []
+})
+
 function Canvas() {
-    const [elements, setElements] = useState<Element[]>([])
-
-    const setElement: SetElement = (indexToSet, newElement) => {
-        setElements(
-            elements.map((element, index) => {
-                if (indexToSet === index) return newElement
-                return element
-            }),
-        )
-    }
-
-    const addElement = () => {
-        setElements((elements) => {
-            return [
-                ...elements,
-                {
-                    style: {
-                        position: {top: 100 + elements.length * 30, left: 100 + elements.length * 30},
-                        size: {width: 100, height: 100},
-                    },
-                },
-            ]
-        })
-    }
-
     const setSelectedElement = useSetRecoilState(selectedElementState);
+    const elements = useRecoilValue(elementsState);
 
     return (
-        <ElementsContext.Provider value={{elements, addElement, setElement}}>
-            <PageContainer
-                onClick={() => {
-                    setSelectedElement(null)
-                }}
-            >
-                <Toolbar />
-                {elements.map((element, index) => (
-                    <Rectangle key={index} element={element} index={index} />
-                ))}
-            </PageContainer>
-        </ElementsContext.Provider>
+        <PageContainer
+            onClick={() => {
+                setSelectedElement(null)
+            }}
+        >
+            <Toolbar />
+            {elements.map((id) => (
+            <Rectangle key={id} id={id} />
+            ))}
+        </PageContainer>
     )
 }
 
